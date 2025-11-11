01b56d17416159052461a8595af4bd9bfeada1bd
ferdinicolas07-ux <ferdinicolas07@gmail.com>
2025-11-11 21:11:10 +0700

    Ferdinand
    Done

--- TFUNC.java
+++ TFUNC.java
@@ -1,27 +1,27 @@
 // 12S25023 - Ferdinand Nicolas Sihombing
 
-mport java.util.*;
+import java.util.*;
 import java.lang.Math;
 
 public class TFUNC {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int angka, hasil;
+        int n, hasil;
 
-        angka = input.nextInt();
-        hasil = hitungRekursif(angka);
-        System.out.println("Bilangan yang dimasukkan :" + angka);
-        System.out.println("Hasil perhitungan rekursif :" + hasil);
+        n = input.nextInt();
+        hasil = pertambahan(n);
+        System.out.println("Bilangan yang dimasukkan : " + n);
+        System.out.println("Hasil perhitungan rekursif : " + hasil);
     }
     
-    public static int hitungRekursif(int angka) {
+    public static int pertambahan(int n) {
         int hasil;
 
-        if (angka == 1) {
+        if (n == 1) {
             hasil = 1;
         } else {
-            hasil = angka + hitungRekursif(angka - 1);
+            hasil = pertambahan(n - 1) + n;
         }
         
         return hasil;
